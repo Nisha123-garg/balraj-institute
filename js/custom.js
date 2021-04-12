28b40adb4d56f76b6d1356d096d3dfa9b851fddf
NIsha <66770907+Nisha123-garg@users.noreply.github.com>
2021-04-12 22:35:13 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+/* $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 25,
+    //    autoWidth: true,
+    items: 3,
+    nav: false,
+    smartSpeed: 1000,
+    autoHeight: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+}) */
+
+
+
+
+$('.owl-carousel').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
